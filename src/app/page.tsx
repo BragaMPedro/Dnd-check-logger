c5c86987b94c239e88764482516d96583c01d13f
BragaMPedro <pedrobmagalhaes95@gmail.com>
2024-02-19 16:33:52 -0300

    layout e metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Form } from "@/components/Form/Form";
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center w-full px-10 sm:pt-4 bg-indigo-950">
+      <main className="flex min-h-screen flex-col items-center w-full bg-indigo-950">
          <Form />
       </main>
    );
