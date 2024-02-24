72a06b3087c7ebf0da478f20dfeee8d75d13d01b
BragaMPedro <pedrobmagalhaes95@gmail.com>
2024-02-23 23:27:07 -0300

    getIndicadores function

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -4,19 +4,19 @@ export function getLog(): Promise<Log[]> {
    const logArray = localStorage.getItem("logArray");
 
    return logArray ? JSON.parse(logArray) : [];
-}
+};
 
 export async function postLog(novoLog: Log) {
    const logArray = await getLog();
    logArray.push(novoLog);
 
    localStorage.setItem("logArray", JSON.stringify(logArray));
-}
+};
 
 export async function postMultipleLogs(novosLogsArray: Log[]) {
 
    localStorage.setItem("logArray", JSON.stringify(novosLogsArray));
-}
+};
 
 export async function deleteLog(index: number) {
    const logArray = await getLog();
@@ -25,4 +25,15 @@ export async function deleteLog(index: number) {
    console.log(deletedItem);
 
    localStorage.setItem("logArray", JSON.stringify(logArray));
+};
+
+export function getIndicadores(): Promise<number> {
+   const indicadores = localStorage.getItem("exportsFalse");
+
+   return indicadores ? JSON.parse(indicadores) : 0;
+}
+
+export async function postIndicadores(indicador: number) {
+
+   localStorage.setItem("exportsFalse", JSON.stringify(indicador));
 }
