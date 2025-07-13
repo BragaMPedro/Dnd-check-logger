a3c5c700aa3efd954771f665b83728154cb93ec1
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-07-13 14:49:47 -0300

    feats: CacheContext

---
+++ src/contexts/CacheContext.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { getAbilityScoresById, getSkillById } from "@/services/dndApi";
+import { AbilityScoreDetailsResponse, SkillDetailsResponse } from "@/types/AbilityScore";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+// Tipagem para o valor do contexto
+interface CacheContextType {
+    getCachedAbilityDetails: (abilityIndex: string) => Promise<AbilityScoreDetailsResponse>;
+    getCachedSkillDetails: (skillIndex: string) => Promise<SkillDetailsResponse>;
+}
+
+// Criando o contexto
+const CacheContext = createContext<CacheContextType | undefined>(undefined);
+
+export const CacheProvider = ({ children }: { children: ReactNode }) => {
+    // Usamos Map para um cache mais performático (O(1) para busca por chave)
+    const [abilitiesCache, setAbilitiesCache] = useState<Map<string, AbilityScoreDetailsResponse>>(new Map());
+    const [skillsCache, setSkillsCache] = useState<Map<string, SkillDetailsResponse>>(new Map());
+
+    // Checa Cache e faz requisição se necessário
+    const getCachedAbilityDetails = async (abilityIndex: string): Promise<AbilityScoreDetailsResponse> => {
+        if (abilitiesCache.has(abilityIndex)) {
+            console.log(`CACHE HIT: '${abilityIndex}'`);
+            return abilitiesCache.get(abilityIndex)!;
+        }
+
+        console.log(`CACHE MISS: '${abilityIndex}'`);
+        const { data } = await getAbilityScoresById(abilityIndex);
+        
+        // Atualiza o cache de forma imutável
+        setAbilitiesCache(prevCache => new Map(prevCache).set(abilityIndex, data));
+        
+        return data;
+    };
+
+    // Função para buscar detalhes de uma Perícia (com cache)
+    const getCachedSkillDetails = async (skillIndex: string): Promise<SkillDetailsResponse> => {
+        if (skillsCache.has(skillIndex)) {
+            console.log(`CACHE HIT: '${skillIndex}'`);
+            return skillsCache.get(skillIndex)!;
+        }
+
+        console.log(`CACHE MISS: '${skillIndex}'`);
+        const { data } = await getSkillById(skillIndex);
+
+        setSkillsCache(prevCache => new Map(prevCache).set(skillIndex, data));
+
+        return data;
+    };
+
+    const value = {
+        getCachedAbilityDetails,
+        getCachedSkillDetails,
+    };
+
+    return (
+        <CacheContext.Provider value={value}>
+            {children}
+        </CacheContext.Provider>
+    );
+};
+
+export const useCacheContext = () => {
+    const context = useContext(CacheContext);
+    if (context === undefined) {
+        throw new Error("useCacheContext deve ser usado dentro de um CacheProvider");
+    }
+    return context;
+};
