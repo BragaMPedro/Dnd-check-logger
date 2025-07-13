34f62051c0f7cc9211da37bbd63fc1f0869569ab
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-07-13 14:50:25 -0300

    implements CacheProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Form } from "@/components/Form/Form";
+import { CacheProvider } from "@/contexts/CacheContext";
 import { LocalStorageProvider } from "@/contexts/LocalStorageContext";
 
 export default function Home() {
    return (
       <LocalStorageProvider>
-         <main className="flex min-h-screen flex-col items-center w-full px-6 py-4 sm:px-10 sm:py-4 bg-indigo-950">
-            <Form />
-         </main>
+         <CacheProvider>
+            <main className="flex min-h-screen flex-col items-center w-full px-6 py-4 sm:px-10 sm:py-4 bg-indigo-950">
+               <Form />
+            </main>
+         </CacheProvider>
       </LocalStorageProvider>
    );
 }
